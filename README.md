10a99aed3b695fc41d3033c479e9d572c094931b
Jonathan Brumley <cayasso@gmail.com>
2013-07-19 00:17:33 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,120 @@
+# Primus Emitter
+
+[![Build Status](https://travis-ci.org/cayasso/primus-emitter.png?branch=master)](https://travis-ci.org/cayasso/primus-emitter)
+[![NPM version](https://badge.fury.io/js/primus-emitter.png)](http://badge.fury.io/js/primus-emitter)
+
+Node.JS module that adds emitter capabilities to a [Primus](https://github.com/3rd-Eden/primus) server.
+
+## Instalation
+
+```
+$ npm install primus-emitter
+```
+
+## Usage
+
+### On the Server
+
+```
+var Primus = require('primus');
+var Emitter = require('primus-emitter');
+var server = require('http').createServer();
+
+// primus instance
+var primus = new Primus(server, { transformer: 'websockets', parser: 'JSON' });
+
+// add emitter to Primus
+primus.use('emitter', Emitter);
+
+primus.on('connection', function (spark) {
+
+  // emit hi event
+  spark.emit('hi', 'good morning');
+
+  // emit to news with ack
+  spark.emit('news', 'good morning', function (data) {
+    console.log(data); // => 'by client'
+  });
+
+  // receive incoming sport messages
+  spark.on('sports', function (data) {
+    console.log('sport', data); // => ping-pong
+  });
+
+});
+
+server.listen(8080);
+```
+
+### On the Client
+
+```
+var primus = Primus.connect('ws://localhost:8080');
+
+primus.on('open', function () {
+
+  // receive incoming hi msgs
+  primus.on('hi', function (data) {
+    console.log(data); // => good morning
+  });
+
+  // respond ack to server
+  primus.on('news', function (data, fn) {
+    fn('by client')
+  });
+
+  // send message to server
+  spark.emit('sport', 'ping-pong');
+
+});
+
+```
+
+## API
+
+### spark#emit(event, ..., [fn])
+
+Emit an event to server to client or client to server.
+
+```
+spark.emit('news', 'hi', fn);
+```
+
+### spark#on(event, fn)
+
+Listen to incoming events.
+
+```
+spark.on('news', fn);
+```
+
+## Run tests
+
+```
+$ make test
+```
+
+## License
+
+(The MIT License)
+
+Copyright (c) 2013 Jonathan Brumley &lt;cayasso@gmail.com&gt;
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ test/test.js
@@ -0,0 +1,136 @@
+var Primus = require('primus');
+var emitter = require('../');
+var http = require('http').Server;
+var expect = require('expect.js');
+var opts = { transformer: 'sockjs', parser: 'JSON' };
+
+
+// creates the client
+function client(srv, primus, port){
+  var addr = srv.address();
+  var url = 'http://' + addr.address + ':' + (port || addr.port);
+  return new primus.Socket(url);
+}
+
+// creates the server
+function server(srv, opts) {
+  // use rooms plugin
+  return Primus(srv, opts).use('emitter', emitter);
+}
+
+describe('primus-emitter', function () {
+
+  it('should have required methods', function(done){
+    var srv = http();
+    var primus = server(srv, opts);
+    srv.listen(function(){
+      primus.on('connection', function (spark) {
+        expect(spark.emit).to.be.a('function');
+        expect(spark.on).to.be.a('function');
+        done();
+      });
+      client(srv, primus);
+    });
+  });
+
+  it('should emit event from server', function(done){
+    var srv = http();
+    var primus = server(srv, opts);
+    srv.listen(function(){ 
+      primus.on('connection', function(spark){
+        spark.emit('news', 'data');
+      });
+      var cl = client(srv, primus);
+      cl.on('news', function (data) {
+        expect(data).to.be('data');
+        done();
+      });
+    });
+  });
+
+  it('should emit object from server', function(done){
+    var srv = http();
+    var primus = server(srv, opts);
+    var msg = { hi: 'hello', num: 123456 };
+    srv.listen(function(){
+      primus.on('connection', function(spark){
+        spark.emit('news', msg);
+      });
+      var cl = client(srv, primus);
+      cl.on('news', function (data) {
+        expect(data).to.be.eql(msg);
+        done();
+      });
+    });
+  });
+
+  it('should support ack from server', function(done){
+    var srv = http();
+    var primus = server(srv, opts);
+    var msg = { hi: 'hello', num: 123456 };
+    srv.listen(function(){
+      primus.on('connection', function(spark){
+        spark.emit('news', msg, function (err, res) {
+          expect(res).to.be('received');
+          expect(err).to.be.eql(null);
+          done();
+        });
+      });
+      var cl = client(srv, primus);
+      cl.on('news', function (data, fn) {
+        fn(null, 'received');
+      });
+    });
+  });
+
+  it('should emit event from client', function(done){
+    var srv = http();
+    var primus = server(srv, opts);
+    srv.listen(function(){
+      primus.on('connection', function(spark){
+        spark.on('news', function (data) {
+          expect(data).to.be('data');
+          done();
+        });
+      });
+      var cl = client(srv, primus);
+      cl.emit('news', 'data');
+    });
+  });
+
+  it('should emit object from client', function(done){
+    var srv = http();
+    var primus = server(srv, opts);
+    var msg = { hi: 'hello', num: 123456 };
+    srv.listen(function(){
+      primus.on('connection', function(spark){
+        spark.on('news', function (data) {
+          expect(data).to.be.eql(msg);
+          done();
+        });
+      });
+      var cl = client(srv, primus);
+      cl.emit('news', msg);
+    });
+  });
+
+  it('should support ack from client', function(done){
+    var srv = http();
+    var primus = server(srv, opts);
+    var msg = { hi: 'hello', num: 123456 };
+    srv.listen(function(){ 
+      primus.on('connection', function(spark){
+        spark.on('news', function (data, fn) {
+          fn(null, 'received');
+        });
+      });
+      var cl = client(srv, primus);
+      cl.emit('news', msg, function (err, res) {
+        expect(res).to.be('received');
+        expect(err).to.be.eql(null);
+        done();
+      });
+    });
+  });
+  
+});
