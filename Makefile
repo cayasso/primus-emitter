d2a44548155855c8463e3f3499cc6dfde733eb5e
Jonathan Brumley <cayasso@gmail.com>
2013-10-31 18:47:55 -0600

    Updating package.json and removing make file

--- Makefile
+++ Makefile
@@ -1,18 +1,8 @@
 REPORTER = spec
-MAIN = index.js
-GLOBAL = PrimusEmitter
-FILE = primus-emitter.js
 
 test:
 	@./node_modules/.bin/mocha \
 		--reporter $(REPORTER) \
 		--bail
 
-build:
-	@./node_modules/.bin/browserbuild \
-		--main $(MAIN) \
-		--global $(GLOBAL) \
-		--basepath lib/ `find lib -name '*.js'` \
-		> $(FILE)
-
-.PHONY: test build
+.PHONY: test
--- index.js
+++ index.js
@@ -2,23 +2,4 @@
  * Module dependencies.
  */
 
-var exp = 'PrimusEmitter = exp;';
-var fs = require('fs');
-var file = __dirname + '/primus-emitter.js';
-var library = fs.readFileSync(file, 'utf-8');
-var PrimusEmitter = require('./lib');
-
-/**
- * Exporting modules.
- */
-
-exports.server = function server(primus, options) {
-  primus.$ = primus.$ || {};
-  primus.$.PrimusEmitter = PrimusEmitter;
-  PrimusEmitter(primus, options);
-};
-
-exports.client = function(){};
-exports.library = library.replace(exp, 'exp(Primus);');
-exports.Emitter = PrimusEmitter.Emitter;
-exports.PrimusEmitter = PrimusEmitter;
+module.exports = require('./lib');
